18f60cdf73fba0e7cd9eac47f4298d45d55998b2
jeeneee <woojin7124@naver.com>
2021-03-13 23:38:55 +0900

    gradle 6.7.1

--- src/test/java/me/jeeneee/springboot/web/HelloControllerTest.java
+++ src/test/java/me/jeeneee/springboot/web/HelloControllerTest.java
@@ -4,19 +4,19 @@ import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import me.jeeneee.springboot.config.auth.SecurityConfig;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class, excludeFilters = {
 	@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)})
 public class HelloControllerTest {
--- src/test/java/me/jeeneee/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/me/jeeneee/springboot/web/dto/HelloResponseDtoTest.java
@@ -2,7 +2,7 @@ package me.jeeneee.springboot.web.dto;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class HelloResponseDtoTest {
 
